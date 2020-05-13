8bf8165e65c807da35ad3414cb9f17854d6e6d04
benkldev <55231890+BenKLDEV@users.noreply.github.com>
2020-05-13 19:44:41 +0200

    Updates

---
+++ precache-manifest.baf41309a1777e0eba4d30554d69d411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41198c497f94b56e72701fc10a9e569",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "c3fdc79e7975d6a09332",
+    "url": "/react-calculator/static/css/main.fa14fd51.chunk.css"
+  },
+  {
+    "revision": "315afb11e367660f1f0c",
+    "url": "/react-calculator/static/js/2.8e0fa6ad.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.8e0fa6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fdc79e7975d6a09332",
+    "url": "/react-calculator/static/js/main.b1b429ca.chunk.js"
+  },
+  {
+    "revision": "7a4e8bb32f6cfb799366",
+    "url": "/react-calculator/static/js/runtime-main.6252dc75.js"
+  }
+]);
